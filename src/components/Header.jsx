09cf37cc21f4404f70a2ce34b4f9507f502c73f7
AlexTomasi-dev <tomasialex.dev@gmail.com>
2024-05-17 16:06:24 +0200

    img

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ function Header (){
                      <h1>ALEX <br /> TOMASI</h1>
                  </div>
                  <div className="col-12  col-lg-6 mia-img text-center my-auto">
-                     <img src="https://plus.unsplash.com/premium_photo-1664304582040-4f6c69c8380c?q=80&w=1336&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Mia foto Alex Tomasi" />
+                     <img src="./assets/mia-img.png" alt="Mia foto Alex Tomasi" />
                  </div>
              </div>
              
